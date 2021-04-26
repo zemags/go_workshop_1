5c944423cd5dc2b9671760c90e75d391e93256d3
zemags <azmt2008@gmail.com>
2021-04-26 16:42:01 +0500

    Add test with mock

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -1,33 +1,58 @@
 package handler
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/go_workshop_1/internal/api"
+	"github.com/go_workshop_1/internal/api/mocks"
 )
 
 func TestHandler_Hello(t *testing.T) {
-	type fields struct {
-		weatherClient api.Client
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name     string
+		weather  *api.WeatherResponse // init field for json
+		err      error
+		codeWant int
+		bodyWant string
 	}{
-		// TODO: Add test cases.
+		{
+			name:     "simple test",
+			weather:  &api.WeatherResponse{Weather: "test weather"},
+			err:      nil,
+			codeWant: 200,
+			bodyWant: "test weather",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := &Handler{
-				weatherClient: tt.fields.weatherClient,
+			apiMock := &mocks.Client{}
+			// when call GetWeather what must return
+			apiMock.On("GetWeather").Return(tt.weather, tt.err)
+
+			h := NewHandler(apiMock)
+
+			req, _ := http.NewRequest("GET", "/hello", nil)
+			rr := httptest.NewRecorder()
+
+			h.Hello(rr, req)
+
+			// same as above line
+			// handler := http.HandlerFunc(h.Hello)
+			// handler.ServeHTTP(rr, req)
+
+			gotRaw, _ := ioutil.ReadAll(rr.Body)
+			got := string(gotRaw)
+
+			if got != tt.bodyWant {
+				t.Errorf("wrong response body %s want %s", got, tt.bodyWant)
 			}
-			h.Hello(tt.args.w, tt.args.r)
+
+			if status := rr.Result().StatusCode; status != tt.codeWant {
+				t.Errorf("wrong response body %d want %d", status, tt.codeWant)
+			}
 		})
 	}
 }
